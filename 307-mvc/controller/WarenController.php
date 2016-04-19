90e7d1a280c8b9f787bd951c8d3b0c8dd57a9b37
stronpassion <varshan@live.de>
2016-04-19 12:32:03 +0200

    Added files via upload

---
+++ 307-mvc/controller/WarenController.php
@@ -0,0 +1,53 @@
+<?php
+
+require_once 'model/UserModel.php';
+
+/**
+ * Siehe Dokumentation im DefaultController.
+ */
+class WarenController
+{
+    public function index()
+    {
+        $warenModel = new WarenModel();
+
+        $view = new View('waren_index');
+        $view->title = 'Waren';
+        $view->heading = 'Waren';
+        $view->waren = $warenModel->readAll();
+        $view->display();
+    }
+
+    public function create()
+    {
+        $view = new View('waren_create');
+        $view->title = 'Waren erstellen';
+        $view->heading = 'Waren erstellen';
+        $view->display();
+    }
+
+    public function doCreate()
+    {
+        if ($_POST['send']) {
+            $firstName = $_POST['name'];
+            $lastName = $_POST['preis'];
+            $email = $_POST['filialenid'];
+            $password = $_POST['menge'];
+
+            $warenModel = new warenModel();
+            $warenModel->create($name, $preis, $filialenid, $menge);
+        }
+
+        // Anfrage an die URI /waren weiterleiten (HTTP 302)
+        header('Location: /waren');
+    }
+
+    public function delete()
+    {
+        $warenModel = new warenModel();
+        $warenModel->deleteById($_GET['id']);
+
+        // Anfrage an die URI /waren weiterleiten (HTTP 302)
+        header('Location: /waren');
+    }
+}
