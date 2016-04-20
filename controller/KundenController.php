72f77a752cd29e0cb0fde709a46d043bb774a87f
stronpassion <varshan@live.de>
2016-04-20 13:51:50 +0200

    Added files via upload

---
+++ controller/KundenController.php
@@ -0,0 +1,59 @@
+<?php
+
+
+require_once 'model/KundenModel.php';
+
+/**
+ * Siehe Dokumentation im DefaultController.
+ */
+
+class KundenController
+{
+    public function index()
+    {
+
+        $kundenModel = new KundenModel();
+
+
+        $view = new View('kunden_index');
+        $view->title = 'Benutzer';
+        $view->heading = 'Benutzer';
+
+        $view->kunden = $kundenModel->readAll();
+        $view->display();
+    }
+
+    public function create()
+    {
+
+        $view = new View('kunden_create');
+        $view->title = 'Benutzer erstellen';
+        $view->heading = 'Benutzer erstellen';
+        $view->display();
+    }
+    public function doCreate()
+    {
+        if ($_POST['login']) {
+
+            $benutzername = $_POST['user'];
+            $passwort = $_POST['pass'];
+
+            $kundenModel = new KundenModel();
+            $kundenModel->create($benutzername, $passwort);
+        }
+
+        // Anfrage an die URI /kunden weiterleiten (HTTP 302)
+        header('Location: /kunden');
+    }
+
+    public function delete()
+    {
+
+        $kundenModel = new KundenModel();
+        $kundenModel->deleteById($_GET['id']);
+
+
+        // Anfrage an die URI /kunden weiterleiten (HTTP 302)
+        header('Location: /kunden');
+    }
+}
