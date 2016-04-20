d6b9e686df1219dc69405e3c7733624e51a3dc57
stronpassion <varshan@live.de>
2016-04-20 13:52:17 +0200

    Added files via upload

---
+++ model/KundenModel.php
@@ -0,0 +1,42 @@
+<?php
+
+require_once 'lib/Model.php';
+
+/**
+ * Das UserModel ist zuständig für alle Zugriffe auf die Tabelle "kunden".
+ *
+ * Die Ausführliche Dokumentation zu Models findest du in der Model Klasse.
+ */
+class KundenModel extends Model
+{
+    /**
+     * Diese Variable wird von der Klasse Model verwendet, um generische
+     * Funktionen zur Verfügung zu stellen.
+     */
+    protected $tableName = 'kunden';
+
+    /**
+     * Erstellt einen neuen benutzer mit den gegebenen Werten.
+     *
+     * Das Passwort wird vor dem ausführen des Queries noch mit dem SHA1
+     *  Algorythmus gehashed.
+     *
+     * @param $benutzername Wert für die Spalte benutzername
+     * @param $passwort Wert für die Spalte passwort
+     *
+     * @throws Exception falls das Ausführen des Statements fehlschlägt
+     */
+    public function create($benutzername, $passwort)
+    {
+        $passwort = sha1($passwort);
+
+        $query = "INSERT INTO $this->tableName (benutzername, passwort) VALUES (?, ?)";
+
+        $statement = ConnectionHandler::getConnection()->prepare($query);
+        $statement->bind_param('ss', $benutzername, $passwort);
+
+        if (!$statement->execute()) {
+            throw new Exception($statement->error);
+        }
+    }
+}
